fcd1039bd7eb526a295b1caa452fd1a9d547f2bb
Eduard Anton <eduardanton78@gmail.com>
2020-01-29 04:06:56 +0700

    Setup main class with junit.

---
+++ src/test/java/com/eddi/MeetingManagerTest.java
@@ -0,0 +1,16 @@
+package com.eddi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static junit.framework.TestCase.assertEquals;
+
+@RunWith(JUnit4.class)
+public class MeetingManagerTest {
+
+    @Test
+    public void myFirstTest() {
+        assertEquals(1, 1);
+    }
+}
