b0076b26549007617dc1e393a54409df7271955a
Eduard Anton <eduardanton78@gmail.com>
2020-01-31 17:46:04 +0700

    Update application.properties

--- src/main/java/com/eddi/controller/IndexController.java
+++ src/main/java/com/eddi/controller/IndexController.java
@@ -6,28 +6,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class GreetingController {
+@RequestMapping("/index")
+public class IndexController {
     @Autowired
     private EmployeeRepo employeeRepo;
 
-    @GetMapping("/greeting")
-    public String greeting(
+    @GetMapping("/index")
+    public String index(
             @RequestParam(name="name", required=false, defaultValue="World") String name,
             Model model
     ) {
         model.addAttribute("name", name);
-        return "greeting";
+        return "index";
     }
 
-    @GetMapping
-    public String main(Model model) {
+    @GetMapping("/create")
+    public String create(Model model) {
         Iterable<Employee> employees = employeeRepo.findAll();
 
         model.addAttribute("employees", employees);
-        return "main";
+        return "create";
     }
 }
 
