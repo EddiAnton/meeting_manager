3f7e4d082d8ffa78c83e3c6979017192d721d71d
Eduard Anton <eduardanton78@gmail.com>
2020-02-02 21:52:21 +0700

    syntax updates.

--- src/main/java/com/eddi/model/Department.java
+++ src/main/java/com/eddi/model/Department.java
@@ -1,6 +1,12 @@
 package com.eddi.model;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import java.util.List;
 
 @Entity
