defe7775104176987c45829d12fe4e5cc197ba8b
Eduard Anton <eduardanton78@gmail.com>
2020-02-27 14:29:17 +0700

    deploy on heroku

--- src/main/java/com/eddi/MeetingManagerApplication.java
+++ src/main/java/com/eddi/MeetingManagerApplication.java
@@ -2,9 +2,16 @@ package com.eddi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MeetingManagerApplication {
+public class MeetingManagerApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MeetingManagerApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MeetingManagerApplication.class, args);
